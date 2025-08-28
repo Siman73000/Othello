a1f6b00988a552a6f228d2c07990357f5f6102b9
Simon Hamilton <si.p.h@outlook.com>
2025-08-28 13:42:05 -0500

    network scanning functionality.

--- OS_Build/Rust-ASM/src/network-driver.rs
+++ OS_Build/Rust-ASM/src/network-driver.rs
@@ -7,5 +7,16 @@ use core::ptr;
 
 pub fn network_Scan() -> ! {
     Port::new(0x52).write(0x00);
-    
+    loop {
+        let status: u8 = Port::new(0x53).read();
+        if status & 0x01 != 0 {
+            let byte1: u8 = Port::new(0x54).read();
+            let byte2: u8 = Port::new(0x55).read();
+            let byte3: u8 = Port::new(0x56).read();
+            let byte4: u8 = Port::new(0x57).read();
+        }
+        if status & 0x02 != 0 {
+            break;
+        }
+    }
 }
